6ad228a664944c5a035a82d27df476534d0d91d7
SudeepKagi <sudeepskagi@gmail.com>
2026-07-16 15:01:40 +0530

    fix(queue): add tls configuration support for secure cloud Redis URLs

--- server/src/queue/connection.js
+++ server/src/queue/connection.js
@@ -2,13 +2,23 @@ import IORedis from "ioredis";
 import { config } from "../config/app.config.js";
 import * as logger from "../services/logger.service.js";
 
+const redisOptions = {
+    maxRetriesPerRequest: null,
+};
+
+if (config.redis.url && config.redis.url.startsWith("rediss://")) {
+    redisOptions.tls = {
+        rejectUnauthorized: false
+    };
+}
+
 const connection = config.redis.url
-    ? new IORedis(config.redis.url, { maxRetriesPerRequest: null })
+    ? new IORedis(config.redis.url, redisOptions)
     : new IORedis({
         host: config.redis.host,
         port: config.redis.port,
         password: config.redis.password,
-        maxRetriesPerRequest: null,
+        ...redisOptions,
     });
 
 connection.on("connect", () => {
